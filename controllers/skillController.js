d25f5681de4883f7d1fab29b192f71c8efc98a67
sakuramarylou121300 <maryloucortez12@gmail.com>
2023-03-14 23:51:00 +0800

    selectInDropDown

--- controllers/skillController.js
+++ controllers/skillController.js
@@ -19,6 +19,12 @@ const createSkills = async(req, res)=>{
         }
 
         for (const skill of skillsToAdd) {
+            //if the value in the drop down is Select
+            if (!skill.skillName || skill.skillName === "Select") {
+                res.status(400).send({ message: "Please enter your skill" });
+                return;
+            }
+            //if the value in the drop down empty
             if (!skill.skillName) {
                 res.status(400).send({ error: "Please enter your skill" });
                 return;
